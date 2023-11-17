a4dba774c33f46ed8db86a39b1618b42c56e2fc6
Vikash <vikash00mot@gmail.com>
2023-11-17 16:14:21 +0530

    learning javascript

---
+++ 1_variables.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version
+
+const a = 1
+let fname = "vikash Kumar"
+var surname = "pandit"
+let state;
+
+
+console.log(a)
+
+fname = "priyanka Kumari"
+surname = "parit"
+
+console.log(fname)
+console.log(surname)
+
+console.table([a,fname,surname,state])
+
+
+
---
+++ 2_datatype.js
@@ -0,0 +1,16 @@
+let age = 21
+let name = "Vikash Kumar"
+let islogged = false
+let state;
+
+// number = > 2 to power 53
+// bigint
+// string => "Vikash"
+//boolean = true/false
+// null = standalone value
+//undefined =  avi value assigned nahi hua hai
+// symbol = > jab uniqueness ka bat kiya jata hai tab symbol ka use hota hai
+//object
+// typeof
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
